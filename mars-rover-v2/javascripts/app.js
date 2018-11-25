4c9db254c53973d610e7ec1052efb0844b9c608b
iondo <ines.ondo.baka@gmail.com>
2018-11-25 19:27:10 +0100

    add grit

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,12 +1,25 @@
-// Rover Object Goes Here
-// ======================
-var rover= {
+
+let rover= {
   direction: "N",
-  x: 0,
-  y: 0,
-  travelLog: []
+  position: {x: 0, y:0},
+  travelLog: [],
 }
-// ======================
+
+let stone= {
+  direction: "E",
+  position: {x: 5, y:5},
+}
+
+let tree= {
+  direction: "w",
+  position: {x: 4, y:0},
+}
+
+let secondRover= {
+  direction: "S",
+  position: {x: 3, y:4},
+}
+
 function turnLeft(rover){
   switch(rover.direction) {
     case "N":
@@ -39,31 +52,65 @@ function turnRight(rover){
   }  
 }
 
-function moveForward(rover){
-  rover.travelLog.push([rover.x, rover.y]);
-  switch(rover.direction) {
-    case "W":
-      rover.x -= 1;
-      break;
-    case "N":
-      rover.y -= 1;
-      break;
-    case "S":
-      rover.y += 1;
-      break;
-    case "E":
-      rover.x += 1;    
+function limit(boundary) {
+  if (boundary < 0) {
+    return 0;
+  } else if(boundary > 9) {
+    return 9;
+  } else {
+    return boundary;
   }
 }
-function execute(list,rover) {
-    for(var i=0 ; i < list.length; i++) {
-      if (list[i] === "f") {
-        moveForward(rover);
-      }  else if (list[i] === "r") {
-        turnRight(rover);
-      }  else if (list[i] ==="l") {
-        turnLeft(rover);   
-      }
+
+function moveForward(rover){
+  rover.travelLog.push([rover.position.x, rover.position.y]);
+  switch(rover.direction) {
+    case "W":
+      rover.position.x = limit(rover.position.x - 1);
+      break;
+    case "N":
+      rover.position.y = limit(rover.position.y- 1);
+      break;
+    case "S":
+      rover.position.y = limit(rover.position.y + 1);
+      break;
+    case "E":
+    rover.position.x = limit(rover.position.x + 1);    
   }
-  console.log(rover.travelLog);
+} 
+
+function moveBackward(rover,obstacle1, obstacle2){
+  rover.travelLog.push([rover.position.x, rover.position.y]);
+  switch(rover.direction) {
+    case "W":
+      rover.position.x= limit(rover.position.x + 1);
+      break;
+    case "N":
+      rover.position.y = limit(rover.position.y + 1);
+      break;
+    case "S":
+      rover.position.y = limit(rover.position.y - 1);
+      break;
+    case "E":
+    rover.position.x = limit(rover.position.y - 1);    
+  }
+} 
+
+function execute(list,rover) {
+  for(var i=0 ; i < list.length; i++) {
+    switch (list[i]) {
+      case "f":
+        moveForward(rover);
+        break;
+       case "r":
+        turnRight(rover);
+        break; 
+       case "l":
+        turnLeft(rover);
+        break;   
+       case "b":
+        moveBackward(rover);   
+    }
+  } 
+console.log(rover.travelLog);
 }
